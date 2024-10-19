9d65f1ea216a27a702fce7ed8825cc3534af28c5
walshj05 <walshj05@moravian.edu>
2024-10-19 18:15:36 -0400

    news instead of new

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function App() {
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <button onClick={createTodo}>+ news</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
